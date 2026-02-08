760a180cdb722e7e1d65f188f6104c754cda1652
sunnynaynay <47611925+sunnynaynay@users.noreply.github.com>
2026-02-08 22:05:59 +1100

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dxc5dcltv/video/upload/v1770546329/Black_Pumas_-_Colors_Official_Audio_xhkyci.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dxc5dcltv/video/upload/v1770548587/Wyatt_Flores_-_Please_Don_t_Go_Official_Audio_fyimlw.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
